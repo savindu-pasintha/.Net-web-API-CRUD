bd2b62880e70d3cc5de0c73b2e0d9cb0387cf6ed
Savindu@Pasintha <64083148+savindu-pasintha@users.noreply.github.com>
2022-09-05 11:51:35 +0530

    Update Student.cs

--- CRUDAPI/Model/Student.cs
+++ CRUDAPI/Model/Student.cs
@@ -3,7 +3,7 @@
     public class Student
     {
         public int Id { get; set; } = 0;
-        public string Name { get; set; } = "";
+        public string Name { get; set; } = "no data";
         public int ROLE { get; set; } = 0;
      
     
